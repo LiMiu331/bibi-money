4f098a12a9b1e76c39126809dad08dff5a283872
limiu <845217637@qq.com>
2020-05-04 15:51:59 +0800

    添加404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,8 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 Vue.use(VueRouter);
-
 const routes: Array<RouteConfig> = [
     {
         path:'/',
@@ -22,8 +22,11 @@ const routes: Array<RouteConfig> = [
     {
         path:'/statistics',
         component:Statistics
+    },
+    {
+        path:'*',
+        component:NotFound
     }
-
 ];
 
 const router = new VueRouter({
