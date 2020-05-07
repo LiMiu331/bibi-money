9ed8e5fdec0f4fd1df4ce978fa94c11e91d75829
limiu <845217637@qq.com>
2020-05-07 18:44:24 +0800

    制作 Labels.vue

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,25 @@
 <template>
     <Layout>
-        <p>你好我是标签</p>
+        <ol>
+            <li>喝可乐 <icon name="money"></icon></li>
+            <li>玩游戏</li>
+            <li>吃饭</li>
+            <li>打车</li>
+        </ol>
+        <div>
+            <button>新建标签</button>
+        </div>
     </Layout>
 </template>
 
-<script>
+<script lang="ts">
     export default {
         name: "Labels",
     }
 </script>
+
+<style lang="scss">
+    @import "~@/assets/styles/helper.scss";
+
+</style>
 
