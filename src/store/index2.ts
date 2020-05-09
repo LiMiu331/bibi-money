bd82c3118d6eb78a9e2d94b20ac5f77b325859e5
limiu <845217637@qq.com>
2020-05-09 19:48:45 +0800

    模块化的Store 删除了 models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,31 +1,11 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
+
 
 const store = {
-    //record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => recordListModel.create(record),
-    //全局变量太多
-    //严重依赖Window
-    //tag store
-    tagList: tagListModel.fetch(),
-    findTag (id: string)  {
-        return this.tagList.filter(t => t.id === id)[0];
-    },
-    createTag: (name: string) => {
-        const message = tagListModel.create(name);
-        if (message === 'duplicated') {
-            window.alert('该标签已创建！🙅');
-        } else if (message === 'success') {
-            window.alert('创建成功！🌟');
-        }
-    },
-    removeTag: (id: string) => {
-        return tagListModel.remove(id);
-    },
-    updateTag: (id: string, name: string) => {
-        return tagListModel.update(id, name);
-    }
+   ...recordStore,
+    ...tagStore
+
 };
 
 export default store;
