66e29571997863db4ddffdf7e3db6815cced2e49
limiu <845217637@qq.com>
2020-05-11 18:42:40 +0800

    完成列表的CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,11 +2,21 @@
     <Layout>
         <tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"></tabs>
         <tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"></tabs>
-        <div>
-            type:{{type}}
-            <br>
-            interval:{{interval}}
-        </div>
+            <ol>
+                <li v-for="(group,index) in result" :key="index">
+                    <h3 class="title">{{group.title}}</h3>
+                    <ol>
+                        <li
+                                v-for="item in group.items" :key="item.id"
+                                class="record"
+                        >
+                           <span>{{tagString(item.tags)}}</span>
+                            <span class="notes">{{item.notes}}</span>
+                            <span>¥{{item.amount}}</span>
+                        </li>
+                    </ol>
+                </li>
+            </ol>
     </Layout>
 </template>
 
@@ -16,30 +26,76 @@
     import Tabs from '@/components/Tabs.vue';
     import intervalList from '@/constants/intervalList';
     import recordTypeList from '@/constants/recordTypeList';
+
+
     @Component({
         components:{Tabs}
     })
     export default  class Statistics extends Vue {
+        tagString(tags: Tag[]){
+            return tags.length === 0 ? '无' :tags.join(',');
+        }
+        get recordList(){
+           return  (this.$store.state as RootState).recordList;
+        }
+        get result(){
+            const { recordList } = this;
+            type HashTableValue = {title: string;items: RecordList[]}
+                const hashTable: {[key: string]: HashTableValue} = {};for(let i = 0;i<recordList.length;i++){
+               const[date,time] = recordList[i].createdAt!.split('T');
+                console.log(date);
+                hashTable[date] = hashTable[date] || {title:date,items:[]};
+                hashTable[date].items.push(recordList[i]);
+            }
+            console.log(hashTable);
+            return hashTable;
+        }
+        mounted(){
+            this.$store.commit('fetchRecords')
+        }
         type = '-';
         interval = 'day';
         intervalList = intervalList;
         recordTypeList = recordTypeList
     }
 </script>
+<style scoped lang="scss">
+    %item{
+        padding: 8px 16px;
+        line-height: 24px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .title{
+       @extend %item
+    }
+    .record{
+        @extend %item
+    }
+    .notes{
+        margin-right: auto;
+        margin-left: 16px;
+        color: #999;
+    }
+</style>
 
 <style scoped lang="scss">
-     ::v-deep .type-tabs-item{
-        background: white;
-        color: #4585F5;
-        &.selected{
-            background: #4585F5;
-            color: white;
-            &::after{
-                display: none;
-            }
-        }
-    }
-    ::v-deep .interval-tabs-item {
-        height: 48px;
-    }
+     ::v-deep {
+         .type-tabs-item{
+             background: white;
+             color: #4585F5;
+             &.selected{
+                 background: #4585F5;
+                 color: white;
+                 &::after{
+                     display: none;
+                 }
+             }
+         }
+         .interval-tabs-item {
+             height: 48px;
+         }
+     }
+
 </style>
